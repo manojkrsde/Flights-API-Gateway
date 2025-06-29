fb1b0c0428c7215b82e21ff1c7b683dfbab0b3a1
Manoj Kumar <manojkumar.ibmce@gmail.com>
2025-06-29 21:24:14 +0530

    FIX: added seeder file for admin user

---
+++ src/seeders/20250629154821-add-admin-user.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const bcrypt = require("bcrypt");
+const { ServerConfig } = require("../config");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "admin",
+          email: "admin@gmail.com",
+          password: bcrypt.hashSync("123456", +ServerConfig.SALT_ROUNDS),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
